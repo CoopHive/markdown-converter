0778b120b70e908ed641e1e2c1ee021b0fd04355
matteoettam09 <matteomanzi09@gmail.com>
2024-12-02 16:48:14 +0100

    fix: docker typo

--- script/test_docker.py
+++ script/test_docker.py
@@ -1,13 +1,13 @@
 import subprocess
 
-build_command = "podman build -t job-image ../docker"
+build_command = "podman build -t job-image -f convert.Dockerfile ../docker"
 subprocess.run(build_command, shell=True, check=True)
 
 conversion_type = "marker"
 input_url = "https://gateway.lighthouse.storage/ipfs/bafkreidsulm4ma4jnkp2zc2zw64rpiqj6diemfimazo4bfkc5urguzql3u"
 
 run_command = (
-    f"podman run --rm --name job-container -v job-image {conversion_type} {input_url}"
+    f"podman run --rm --name job-container job-image {conversion_type} {input_url}"
 )
 result = subprocess.run(
     run_command,
