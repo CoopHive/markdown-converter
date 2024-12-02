8c1d7d445b66d730de72a54ac35f7d0bb2112b6e
matteoettam09 <matteomanzi09@gmail.com>
2024-12-02 17:34:37 +0100

    fix: codestyle

--- script/test_docker.py
+++ script/test_docker.py
@@ -5,9 +5,9 @@ from descidb import converter
 conversion_type = "marker"
 input_url = "https://gateway.lighthouse.storage/ipfs/bafkreidsulm4ma4jnkp2zc2zw64rpiqj6diemfimazo4bfkc5urguzql3u"
 
-# converted = converter.convert_from_url(
-#     conversion_type=conversion_type, input_url=input_url
-# )
+converted = converter.convert_from_url(
+    conversion_type=conversion_type, input_url=input_url
+)
 
 build_command = "podman build -t job-image -f ../docker/convert.Dockerfile"
 subprocess.run(build_command, shell=True, check=True)
