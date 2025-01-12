c752042e1f63dfb013497f4aad7834d60d6a6b8d
Vardhan Shorewala <vardhanshorewala@Vardhans-MacBook-Air.local>
2025-01-12 21:34:37 +0530

    Adding Pipeline for Chunking

--- script/test_docker.py
+++ script/test_docker.py
@@ -1,56 +1,79 @@
 import subprocess
 
-from descidb import converter
 from descidb import chunker
+import json
+import ast
 
-if False:
-    conversion_type = "marker"
-    input_url = "https://gateway.lighthouse.storage/ipfs/bafkreidsulm4ma4jnkp2zc2zw64rpiqj6diemfimazo4bfkc5urguzql3u"
+# def test_pipeline(config: dict, input_url: str) -> None:
+#     """Test the pipeline with the specified configuration and input URL."""
+#     # Convert the input URL
+#     conversion_type = config["converter"]
+#     chunking_type = config["chunker"]
+#     embedding_type = config["embedder"]
 
-    converted = converter.convert_from_url(
-        conversion_type=conversion_type, input_url=input_url
-    )
+#     build_command = "podman build --no-cache -t job-image -f ../docker/convert.Dockerfile"
+#     subprocess.run(build_command, shell=True, check=True)
 
-    build_command = "podman build --no-cache -t job-image -f ../docker/convert.Dockerfile"
-    subprocess.run(build_command, shell=True, check=True)
+#     remove_command = "podman rm -f job-container"
+#     subprocess.run(remove_command, shell=True, check=False)
 
-    remove_command = "podman rm -f job-container"
-    subprocess.run(remove_command, shell=True, check=False)
+#     run_command = (
+#         f"podman run --rm --name job-container job-image {
+#             conversion_type} {input_url}"
+#     )
+#     try:
+#         result = subprocess.run(
+#             run_command,
+#             shell=True,
+#             check=True,
+#             capture_output=True,
+#             text=True,
+#         )
+#         result = result.stdout
+#     except subprocess.CalledProcessError as e:
+#         print("Command failed:", e.stderr)
 
-    run_command = (
-        f"podman run --rm --name job-container job-image {conversion_type} {input_url}"
-    )
-    try:
-        result = subprocess.run(
-            run_command,
-            shell=True,
-            check=True,
-            capture_output=True,
-            text=True,
-        )
-        result = result.stdout
+#     # Chunk the converted text
+#     build_command = "podman build --no-cache -t job-image -f ../docker/chunk.Dockerfile"
+#     subprocess.run(build_command, shell=True, check=True)
 
-        print(result)
-    except subprocess.CalledProcessError as e:
-        print("Command failed:", e.stderr)
+#     remove_command = "podman rm -f job-container"
+#     subprocess.run(remove_command, shell=True, check=False)
+
+#     run_command = (
+#         f"podman run --rm --name job-container job-image {
+#             chunking_type} {result}"
+#     )
+
+#     try:
+#         result = subprocess.run(
+#             run_command,
+#             shell=True,
+#             check=True,
+#             capture_output=True,
+#             text=True,
+#         )
+#         result = result.stdout
+#     except subprocess.CalledProcessError as e:
+#         print("Command failed:", e.stderr)
 
 
-embeder_type = "openai"
-ipfs_url = "https://gateway.lighthouse.storage/ipfs/bafkreies5jikyxatomqj2zrg5e7z2fpb5bd62zsgqqhkd2k5eorhy5jc2i"
+chunker_type = "sentence"
+input_url = "https://gateway.lighthouse.storage/ipfs/bafkreies5jikyxatomqj2zrg5e7z2fpb5bd62zsgqqhkd2k5eorhy5jc2i"
 
-if False:
-    chunked = chunker.chunk_from_url(
-        chunker_type=chunker_type, input_url=ipfs_url)
-    print(chunked)
+converted = chunker.chunk_from_url(
+    chunker_type=chunker_type, input_url=input_url
+)
 
-build_command = "podman build --no-cache -t job-image -f ../docker/embed.Dockerfile"
+build_command = "podman build --no-cache -t job-image -f ../docker/chunk.Dockerfile"
 subprocess.run(build_command, shell=True, check=True)
 
 remove_command = "podman rm -f job-container"
 subprocess.run(remove_command, shell=True, check=False)
 
 run_command = (
-    f"podman run --rm --name job-container job-image {embeder_type} {ipfs_url}"
+    f"podman run --rm --name job-container job-image {
+        chunker_type} {input_url}"
 )
 try:
     result = subprocess.run(
@@ -60,8 +83,52 @@ try:
         capture_output=True,
         text=True,
     )
-    result = result.stdout
+    raw_output = result.stdout.strip()
+    print(raw_output)
 
-    print(result)
+    result_array = ast.literal_eval(raw_output)
+
+    print(result_array[0])
+    print(result_array)
 except subprocess.CalledProcessError as e:
     print("Command failed:", e.stderr)
+
+
+# embeder_type = "openai"
+# ipfs_url = "https://gateway.lighthouse.storage/ipfs/bafkreies5jikyxatomqj2zrg5e7z2fpb5bd62zsgqqhkd2k5eorhy5jc2i"
+
+# if False:
+#     chunked = chunker.chunk_from_url(
+#         chunker_type=chunker_type, input_url=ipfs_url)
+#     print(chunked)
+
+# build_command = "podman build --no-cache -t job-image -f ../docker/chunk.Dockerfile"
+# subprocess.run(build_command, shell=True, check=True)
+
+# remove_command = "podman rm -f job-container"
+# subprocess.run(remove_command, shell=True, check=False)
+
+# env_vars = {
+#     "OPENAI_API_KEY": "sk-DkpEd31FQDPP4hycFq743IIT-QlFjXiRBeYZ7PVWWdT3BlbkFJl2elt0mENPhg4d7vlEFGucjoJ5rC_qGO3GsuP4ARsA",
+# }
+
+# env_flags = " ".join(f"-e {key}={value}" for key, value in env_vars.items())
+
+# run_command = (
+#     f"podman run --rm --name job-container {
+#         env_flags} job-image {embeder_type} {ipfs_url}"
+# )
+
+# try:
+#     result = subprocess.run(
+#         run_command,
+#         shell=True,
+#         check=True,
+#         capture_output=True,
+#         text=True,
+#     )
+#     result = result.stdout
+
+#     print(result)
+# except subprocess.CalledProcessError as e:
+#     print("Command failed:", e.stderr)
