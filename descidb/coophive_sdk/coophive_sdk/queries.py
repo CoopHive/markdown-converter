92d4d65217fae3779d8770a064b52ca85147d162
Vardhan Shorewala <vardhanshorewala@berkeley.edu>
2025-04-04 13:10:47 -0700

    Moved API/SDK to branch and cleaned main head

--- descidb/coophive_sdk/coophive_sdk/queries.py
+++
@@ -1,9 +0,0 @@
-class Queries:
-    def __init__(self, client):
-        self.client = client
-
-    def query_database(self, collection_name, user_query):
-        data = {"user_query": user_query}
-        return self.client._request(
-            "POST", f"/collections/{collection_name}/query", json=data
-        )
